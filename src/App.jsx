7e520f94fca0dc8cb9e8df0102b30d5973397855
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-02-09 06:51:27 +0000

    minor fix on styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,49 @@ import { HStack, Input, VStack, FormControl, FormLabel, Button } from '@chakra-u
 function App() {
 
   return (
-    <div className="bg-white shadow-md p-10 w-[400px] mx-auto flex justify-center items-start flex-col gap-2 text-left">
+    <div className="bg-white shadow-md my-10 p-10 w-[400px] mx-auto flex justify-center items-start flex-col gap-2 text-left">
       <h1 className='font-bold text-2xl'>Checkout Page</h1>
       <VStack className="text-left items-start">
-        <p className='text-left'>Personal Details</p>
+        <p className='flex self-start'>Personal Details</p>
         <HStack>
           <FormControl>
             <FormLabel>Firstname</FormLabel>
-            <Input type='text' />
+            <Input type='text' placeholder='firstname' />
           </FormControl>
           <FormControl>
             <FormLabel>Lastname</FormLabel>
-            <Input type='text' />
+            <Input type='text' placeholder='Lastname' />
           </FormControl>
         </HStack>
         <FormControl>
           <FormLabel>Email</FormLabel>
-          <Input type='email' />
+          <Input type='email' placeholder='johndoe@gmail.com' />
         </FormControl>
       </VStack>
       <VStack className="">
-        <p>Billing Details</p>
+        <p className='flex self-start'>Billing Details</p>
         <FormControl>
           <FormLabel>Name on Card</FormLabel>
-          <Input type='email' />
+          <Input type='text' placeholder='John Doe' />
         </FormControl>
         <FormControl>
           <FormLabel>Card Number</FormLabel>
-          <Input type='email' />
+          <Input type='text' placeholder='1234567890'/>
         </FormControl>
         <HStack>
           <FormControl>
             <FormLabel>Expiry Date</FormLabel>
-            <Input type='email' />
+            <Input type='text' placeholder='Expiry' />
           </FormControl>
           <FormControl>
             <FormLabel>CVV</FormLabel>
-            <Input type='email' />
+            <Input type='text' placeholder='123' />
           </FormControl>
         </HStack>
       </VStack>
-      <VStack className="">
-        <p>Have a Coupon?</p>
-        <FormControl>
+      <VStack className='w-full'>
+        <p className='flex self-start'>Have a Coupon?</p>
+        <FormControl >
           <FormLabel>Coupon</FormLabel>
           <Input type='text' />
         </FormControl>
